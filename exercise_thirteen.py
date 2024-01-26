248d07a045c6351e1f37f3c84585cda36b36c582
Angela Ferrer <gelferrero01@gmail.com>
2024-01-26 21:41:56 +0800

    creating inner and outer loop

--- exercise_thirteen.py
+++ exercise_thirteen.py
@@ -2,3 +2,9 @@
 
 # Print display message
 print ("Multiplication Table (1-10)")
+
+# Create outer loop displaying numbers 1 to 10
+for i in range (1,11):
+    # Create inner loop to iterate ten times as well
+    for j in range (1,11):
+        print ( i * j)
