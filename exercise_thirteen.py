4eb9f776d3059dd1bdcc0f49f4a2940637f36f36
Angela Ferrer <gelferrero01@gmail.com>
2024-01-26 21:47:56 +0800

    Finalized

--- exercise_thirteen.py
+++ exercise_thirteen.py
@@ -1,12 +1,12 @@
 # Print multiplication table from 1 to 10
 
 # Print display message
-print ("Multiplication Table (1-10)")
+print ("Multiplication Table (1-10)""\n")
 
 # Create outer loop displaying numbers 1 to 10
 for i in range (1,11):
     # Create inner loop to iterate ten times as well
     for j in range (1,11):
-        print ( i * j)
-    # Add new tab to display result properly
-        print ("\t")
+        print ( i * j, end= " ")
+    # Add space to display result properly
+    print ("\n")
